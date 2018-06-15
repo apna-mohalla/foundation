1db8c6d44072cbfcc2445bac2b16e1ff34fa9bf4
Vivek Kumar <kvivek@thoughtworks.com>
2018-06-15 14:44:52 +0530

    Vivek | Setting Up spring framework

---
+++ src/main/java/apna/Maholla/MahollaApplication.java
@@ -0,0 +1,12 @@
+package apna.Maholla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahollaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahollaApplication.class, args);
+	}
+}
